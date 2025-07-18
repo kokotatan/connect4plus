1c8be1ec678fce8574e81384e7265534e721b065
kokotatan <okuyama.kotaro@gmail.com>
2025-07-18 09:14:09 +0900

    Remove basePath to fix 404 error on Vercel default URL

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' && process.env.VERCEL_ENV === 'production' ? '/game/connect4plus' : '',
-  basePath: process.env.NODE_ENV === 'production' && process.env.VERCEL_ENV === 'production' ? '/game/connect4plus' : '',
   trailingSlash: true,
   images: {
     unoptimized: true
