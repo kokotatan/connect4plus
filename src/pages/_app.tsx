71742d20013ffe25139d416ac133a8da556870e5
kokotatan <okuyama.kotaro@gmail.com>
2025-09-15 17:58:18 +0900

    feat: ファビコンをゲームアイコンに変更
    - connect4pluslogo.pngをベースにファビコンを作成
    - 複数サイズのファビコンファイルを生成 (16x16, 32x32, 1024x1024)
    - _app.tsxにHeadコンポーネントを追加してファビコンを設定
    - ブラウザタブとブックマークにゲームアイコンが表示されるように

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
 import '../globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { BGMProvider } from '../contexts/BGMContext'
 import { ThemeProvider } from '../contexts/ThemeContext'
  
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <BGMProvider>
-        <Component {...pageProps} />
-      </BGMProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Connect4Plus - 次世代型立体四目並べ</title>
+        <meta name="description" content="Connect4Plusは次世代型立体四目並べゲームです。オンライン対戦、AI対戦、オフライン対戦が楽しめます。" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+      </Head>
+      <ThemeProvider>
+        <BGMProvider>
+          <Component {...pageProps} />
+        </BGMProvider>
+      </ThemeProvider>
+    </>
   )
 } 
